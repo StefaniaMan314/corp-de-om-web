4d43a441a90144dd2c56fedac39df0372e1ee14f
stefi <stefaniaman314@gmail.com>
2022-07-30 11:01:39 +0300

    Create poem component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PoemComponent } from './poem/poem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PoemComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
